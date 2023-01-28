527e57b8656216a863299b28f95c9612707b4f52
Nikolay <agoncharov@gmail.com>
2023-01-28 22:45:11 +0300

    moved to gtest

--- test_version.cpp
+++ test_version.cpp
@@ -1,14 +1,11 @@
-#define BOOST_TEST_MODULE test_version
-
 #include "lib.h"
-
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version)
-{
-    BOOST_CHECK(version() > 0);
+#include <gtest/gtest.h>
+ 
+TEST(VesrionTest, IsPositive) {
+    ASSERT_GT(version(), 0);
 }
-
-BOOST_AUTO_TEST_SUITE_END()
+ 
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
