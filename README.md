7f04d0bb92d4ab9ef25cf811815e359d594c8510
Nikolay <agoncharov@gmail.com>
2023-05-18 15:01:59 +0300

    improve code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # otuscpp
-Homeworks for the course:
-OTUS. C++ Developer. Professional.
+# Homeworks for the course:
+# OTUS. C++ Developer. Professional.
+
+[You can find documentation here.](https://nik-eastman.github.io/otuscpp)
--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
+/*!
+\file
+Homework 04 for OTUS course: C++ Developoer Professional.
+*/
+
 #include <iostream>
 #include <vector>
 #include <list>
 #include <tuple>
 
-// delimiter for IP printing
+
+/// delimiter, used for for IP printing
 const char* delimiter = ".";
 
 /////////////////// integer types support /////////////////////
 
-// print IP only for integer types
+/// Print IP only for integer types
 template<typename T, typename = std::enable_if_t<
     std::is_integral<T>::value &&  // consider all intgral types
     (not std::is_same_v<T,bool>)   // excluding 'bool' type
@@ -20,12 +26,11 @@ void print_ip(T v) {
         if(i != 1)
             std::cout << delimiter;
     }
-    std::cout << std::endl;
 }
 
 /////////////////// std::vector, std::list support /////////////////////
 
-// print IP for std::list and std::vector types
+/// Print IP for std::list and std::vector types
 template <
 	template <typename, typename> typename Container,
 	typename Type,
@@ -39,47 +44,45 @@ void print_ip(const Container<Type, Allocator>& v) {
             std::cout << delimiter;
         std::cout << *it;
     }
-    std::cout << std::endl;
 }
 
 /////////////////// std::string support /////////////////////
 
-// print IP for string type
-// detect std::string type by c_str() method
-template <typename T>
-auto print_ip(const T& v) -> decltype (v.c_str(), void()) {
-    std::cout << v.c_str() << std::endl;
+/// Print IP for string type
+template <typename T, typename = std::enable_if_t<std::is_same_v<T, std::string>>>
+void print_ip(const T& v) {
+    std::cout << v;
 }
 
 /////////////////// std::tuple support /////////////////////
 
-// check if std::tuple holds only given type 
+/// Helper for tuple_holds_only 
 template <typename, typename>
 struct tuple_holds_only {};
 
+/// Check if std::tuple holds only given type 
 template <typename ...A, typename B>
 struct tuple_holds_only<std::tuple<A...>, B>
     : std::bool_constant<(std::is_same_v<A, B> && ...)>
 {};
 
-// check if std::tuple holds values of the same types
 template <typename... A>
 struct is_monotype_tuple {};
 
+/// Check if std::tuple holds values of the same types
 template <typename A, typename ...B>
 struct is_monotype_tuple<std::tuple<A, B...>>
     : tuple_holds_only<std::tuple<B...>, A>
 {};
 
-// prints one std::tuple value by index
+/// Prints one std::tuple value by index
 template<typename tuple_type, size_t... I>
 void print_tuple(const tuple_type& v, std::index_sequence<I...>)
 {
     (..., (std::cout << (I == 0 ? "" : delimiter) << std::get<I>(v)));
-    std::cout << std::endl;
 }
 
-// print IP from std::tuple only if all types in std::tuple are the same
+/// Print IP from std::tuple only if all types in std::tuple are the same
 template<
         template <typename...> typename tuple_type,
         typename... T, 
@@ -93,12 +96,20 @@ void print_ip(const tuple_type<T...>& v)
 
 int main (int, char **) {
     print_ip( int8_t{-1} ); // 255
+    std::cout << std::endl;
     print_ip( int16_t{0} ); // 0.0
+    std::cout << std::endl;
     print_ip( int32_t{2130706433} ); // 127.0.0.1
+    std::cout << std::endl;
     print_ip( int64_t{8875824491850138409} ); // 123.45.67.89.101.112.131.41
+    std::cout << std::endl;
     print_ip( std::string{"Hello, World!"} ); // Hello, World!
+    std::cout << std::endl;
     print_ip( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400
+    std::cout << std::endl;
     print_ip( std::list<short>{400, 300, 200, 100} ); // 400.300.200.100
+    std::cout << std::endl;
     print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
+    std::cout << std::endl;
     return 0;
 }
